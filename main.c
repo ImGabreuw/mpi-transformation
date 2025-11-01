a7a0767947e356e0e59dff2e091d885b9151ac2d
ImGabreuw <imgabreuw.dev@gmail.com>
2025-11-01 13:28:02 -0300

    Refactor print_vector function to use variadic arguments for formatting

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
 #include "mpi.h"
 
 #define DATA_SIZE 100
 #define NUM_PROCESSES 5
 #define ROOT_PROCESS 0
 
-void print_vector(const char* label, int* vector, int size) {
-    printf("%s: [", label);
+void print_vector(int* vector, int size, const char* mask, ...) {
+    va_list args;
+    va_start(args, mask);
+    vprintf(mask, args);
+    va_end(args);
+    printf(": [");
     for (int i = 0; i < size; i++) {
         printf("%d", vector[i]);
         if (i < size - 1) {
@@ -45,6 +51,7 @@ int main(int argc, char* argv[]) {
     if (rank == ROOT_PROCESS) {
         global_data = (int*)malloc(DATA_SIZE * sizeof(int));
         gathered_data = (int*)malloc(DATA_SIZE * sizeof(int));
+
         if (global_data == NULL || gathered_data == NULL) {
             fprintf(stderr, "[Process %d] Failed to allocate memory for global data.\n", ROOT_PROCESS);
             free(global_data);
@@ -55,10 +62,12 @@ int main(int argc, char* argv[]) {
         for (int i = 0; i < DATA_SIZE; i++) {
             global_data[i] = i + 1;
         }
-        print_vector("[Process 0] Original Vector", global_data, DATA_SIZE);
+
+        print_vector(global_data, DATA_SIZE, "[Process %d] Original Vector", ROOT_PROCESS);
     }
 
     MPI_Scatter(global_data, chunk_size, MPI_INT, local_data, chunk_size, MPI_INT, ROOT_PROCESS, MPI_COMM_WORLD);
+    print_vector(local_data, chunk_size, "[Process %d] Received Chunk", rank);
 
     for (int i = 0; i < chunk_size; i++) {
         local_data[i] = local_data[i] * local_data[i];
@@ -67,7 +76,7 @@ int main(int argc, char* argv[]) {
     MPI_Gather(local_data, chunk_size, MPI_INT, gathered_data, chunk_size, MPI_INT, ROOT_PROCESS, MPI_COMM_WORLD);
 
     if (rank == ROOT_PROCESS) {
-        print_vector("[Process 0] Transformed Vector", gathered_data, DATA_SIZE);
+        print_vector(gathered_data, DATA_SIZE, "[Process %d] Transformed Vector", ROOT_PROCESS);
         free(global_data);
         free(gathered_data);
     }
